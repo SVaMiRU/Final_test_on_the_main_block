76949788da0ed111c36b1e62ecbe1933b7c3a0ce
SVaMiRU <149845345+SVaMiRU@users.noreply.github.com>
2024-04-29 06:56:07 +0300

    Добавление ввода и цикла

--- Controlnaya.py
+++ Controlnaya.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+
+def filter_short_strings(input_array):
+    output_array = []
+    for string in input_array:
+        if len(string) <= 3:
+            output_array.append(string)
+    return output_array
